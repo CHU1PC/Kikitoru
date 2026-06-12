fec708152f8dd085c244e9e874a7a34392cfb0de
CHU1PC <raikyu70@gmail.com>
2026-06-12 22:25:01 +0900

    improve: drop unused oauth env defaults from test conftests

--- backend/tests/integration/conftest.py
+++ backend/tests/integration/conftest.py
@@ -11,9 +11,6 @@ load_dotenv(_PROJECT_ROOT / ".env")
 os.environ.setdefault("HF_TOKEN", "")
 os.environ.setdefault("DATABASE_URL", "postgresql+asyncpg://unused:unused@localhost/unused")
 os.environ.setdefault("GOOGLE_API_KEY", "unused")
-os.environ.setdefault("GOOGLE_CLIENT_ID", "unused")
-os.environ.setdefault("GOOGLE_CLIENT_SECRET", "unused")
-os.environ.setdefault("GOOGLE_REDIRECT_URI", "http://localhost/unused")
 os.environ.setdefault("LLM_TIMEOUT_SECONDS", "30")
 
 
